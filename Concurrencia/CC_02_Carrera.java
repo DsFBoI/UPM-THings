1fe84b9e2b0b7abdc432f0b193fb854fd36e467c
DsFBoI <98948381+DsFBoI@users.noreply.github.com>
2022-02-08 12:11:30 +0100

    Update CC_02_Carrera.java

--- Concurrencia/CC_02_Carrera.java
+++ Concurrencia/CC_02_Carrera.java
@@ -1,48 +1,65 @@
 public class CC_02_Carrera{
-    static int n = 0;   //Variables in which the operations are going to be done
-    static int M = 10;  //
-    static int N = 20;
-    public static class Incr extends Thread{//Increasing Thread
+    static int n = 0;   //Variable in which the operations are going to be done
+    static int M = 10;  //Number of times that we want to do the Threads
+    static int N = 2;   //Number of Increments and Drecreces we are going to do
+    /*---------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+    //Increasing Thread
+
+    public static class Incr extends Thread{
         int val;
         public Incr(int val){
-            this.val = val;
+            this.val = val;     //gives the thread a value
         }
         public void run(){
-            n++;
+            n++;                //Modification of run
 
         }
      }
+     
+    /*---------------------------------------------------------------------------------------------------------------------------------------------------------*/       
+     //Decreasing Thread
+
     public static class Decr extends Thread{//Decreasing Thread
         int val;
         public Decr(int val){
-            this.val = val;
+            this.val = val;     //gives the thread a value
         }
 
-        public void run(){
+        public void run(){      //Modification of run
             n--;
 
         }
     }
+    /*---------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+    // Main
     public static void main(String[] args) {
-        int i = 0;
-        while(i < M ){
+        for(int i = 0 ; i < M ; i++){
             Thread incr = new Thread();
             Thread decr = new Thread();
             for(int j = 0;j < N ; j++ ){
-                incr = new Thread(new Incr(j));;
-                decr = new Thread(new Decr(j));;
-                incr.start();
+                incr = new Thread(new Incr(j));//Giving the threads the values of the thread they are on
+                decr = new Thread(new Decr(j));
+                incr.start();  //Start the thread with the run option
                 decr.start();
                 
                
             }
 
-            
-            i++;
-        }
-
-       
-        System.out.println(n);
+            for(int j = 0;j < N ; j++ ){
+                try{//we have to do join to see if there is any exception on the threads when doind the previous process
+                    incr.join();
+                    decr.join();
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                    System.out.println("\"ERROR\"");
+                }
+            }
+    
+        }       
+        System.out.println("n = " + n);
         
     }
 }
