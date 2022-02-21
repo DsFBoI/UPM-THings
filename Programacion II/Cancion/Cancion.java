410318d540041a92c705c493e51dada2a5862e6d
DsFBoI <98948381+DsFBoI@users.noreply.github.com>
2022-02-21 10:06:17 +0100

    Create Cancion.java

---
+++ Programacion II/Cancion/Cancion.java
@@ -0,0 +1,14 @@
+public class Cancion {
+    String Titulo;
+    String artist;
+    String Album;
+    int Duracionsec;
+    String Type;
+    Cancion (String Titulo,String artist,String Album,int Duracionsec,String Type){
+        this.Titulo = Titulo;
+        this.artist = artist;
+        this.Album = Album;
+        this.Duracionsec = Duracionsec;
+        this.Type = Type;
+    }
+}
