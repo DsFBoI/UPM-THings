456c015e1686ba25e1aa6584a418511505f94de0
DsFBoI <98948381+DsFBoI@users.noreply.github.com>
2022-12-28 17:17:36 +0100

    Update maxmin.c

--- PPS/Trabajos/2k22/maxmin.c
+++ PPS/Trabajos/2k22/maxmin.c
@@ -1,36 +1,44 @@
-
 #include <stdio.h>
 
-
-int main(int argc, char *argv[] ){
-    if(argc != 2){
-        return -1;
-    }
-    FILE *g;
-    g = fopen(argv[1],"r");
-    if(g = NULL){
-        return -1;
-    }
-    float max, min,num;
-    max= 0;
-    min = 0;
-    num = 0;
-
-    if(fscanf( "%f",&num)==1){
-
-        max = num;
-        min = num;
-    }
-    printf("%10.2f",num);
-
-    while(fscanf( "%f",&num)==1){
-        if(num > max){
-            max = num;
-        }
-        if(num <min){
-            min = num;
-        }
-    }
-    printf("%10.2f %10.2f",&max,&min);
-    return 0;
+int main(int argc, char **argv)
+{
+	FILE *g;
+	g = fopen(argv[1],"r");
+	
+	if(argc !=2){
+		return (-1);
+	}
+	
+	
+		if(g !=NULL){
+			
+			float min,max,aux;
+			
+			if(fscanf(g, "%f",&max) == 0){
+				max = 0;
+				min = 0;
+			printf("%10.2f%10.2f\n",max, min);
+			return 0;
+			}
+			min = max;
+			 
+			while(fscanf(g, "%f",&aux)==1){
+				if(max > aux && min > aux){
+					min = aux;
+				}
+				if(aux>max){
+					max = aux;
+				}
+				
+			}
+			printf("%10.2f%10.2f\n",max, min);
+		
+		}
+		else{
+			return (-1);
+		}
+			
+	
+	
+	return 0;
 }
